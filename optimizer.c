4f4b67b8d04dc13681e904f5fdacf6927bca9376
xavier wallis <wallisxdevelopment@gmail.com>
2024-12-06 19:34:10 -0500

    added loop just to debug and play

--- optimizer.c
+++ optimizer.c
@@ -1,4 +1,11 @@
 #include "optimizer.h"
 #include "parser.h"
 
-void optimize(Line *lines) { return; }
+void optimize(Line *lines) { 
+  Line* line;
+  size_t index = 0;
+  while ( ( line = *(lines + index) ) != NULL ) {
+    printf("%s", line.var );
+  }
+  return; 
+}
