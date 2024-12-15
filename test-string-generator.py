6cdd35f7c00014b87eeb7002d18f748a964e3589
Vasyl Bodnar <vasyl_bodnar@outlook.com>
2024-12-14 22:17:34 -0500

    Added extra large test case

--- test-string-generator.py
+++ test-string-generator.py
@@ -7,6 +7,7 @@ letter = lambda: choice( alphabet )
 
 dual = [ f'{ letter1 }{ letter2 }' for letter1, letter2 in zip( alphabet, reversed( alphabet ) ) ]
 triple = [ f'{ letter() }{ letter() }{ letter() }' for _ in range(5200) ] 
+quadruple = [ f'{ letter() }{ letter() }{ letter() }{ letter() }' for _ in range(20530) ]
 
 with open( 'input-s.txt', 'w+' ) as file:
   [ file.write( f'{ letter } { int( random() * 10 ) }\n' ) for letter in alphabet ]
@@ -18,3 +19,10 @@ with open( 'input-l.txt', 'w+' ) as file:
   [ file.write( f'{ duo } { choice("+-/*") } { letter() } { letter() }\n' ) for duo in dual ]
   [ file.write( f'{ trio } { choice("+-/*") } { choice(dual) } { choice(dual) }\n' ) for trio in triple]
   file.write( f'R { choice(triple) }' ) 
+
+with open( 'input-xl.txt', 'w+' ) as file:
+  [ file.write( f'{ letter } { int( random() * 10 ) }\n' ) for letter in alphabet ]
+  [ file.write( f'{ duo } { choice("+-/*") } { letter() } { letter() }\n' ) for duo in dual ]
+  [ file.write( f'{ trio } { choice("+-/*") } { choice(dual) } { choice(dual) }\n' ) for trio in triple]
+  [ file.write( f'{ quad } { choice("+-/*") } { choice(triple) } { choice(triple) }\n' ) for quad in quadruple]
+  file.write( f'R { choice(quadruple) }' )
