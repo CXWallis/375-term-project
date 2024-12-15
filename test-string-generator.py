e781be00266d3da658eb2c7100448fffe6c5fd3c
xavier wallis <wallisxdevelopment@gmail.com>
2024-12-14 20:56:52 -0500

    added final R <variable> to the test string

--- test-string-generator.py
+++ test-string-generator.py
@@ -10,3 +10,4 @@ with open( 'input.txt', 'w+' ) as file:
   [ file.write( f'{ letter1 }{ letter2 } { choice("+-/*") } { letter() } { letter() }\n' ) for letter1, letter2 in zip( alphabet, reversed( alphabet ) ) ]
   for i in range( 2200 ):
     file.write( f'{ letter() }{ letter() }{ letter() } {choice("+-/*")} { letter() }{ letter() } { letter() }{ letter() }\n' )
+  file.write( f'R { letter() }{ letter() }' ) 
